5bef1d2c7815f2091c11cf0cbd9664a970999b86
LE DUC TAI-DEV <ductaidn99@gmail.com>
2024-10-30 23:38:08 +0700

    update

--- script.js
+++ script.js
@@ -17,35 +17,68 @@ function createInitials(name) {
 
 const fullName = "Lê đức tài";
 const initials = createInitials(fullName);
+function handleClick() {
+    const inputElement = document.getElementById("userInput");
+    inputElement.value = "";
+    inputElement.focus()
+    showData();
+}
 
 function handleInputChange(event) {
-    const inputValue = event.target.value.split(" ");
-    const houseNumer = inputValue[0];
-    const streetName = inputValue.slice(1).join(" ");
-    console.log("Dữ liệu nhập:", createInitials(streetName));
+    document.querySelector(".loading").innerHTML = `<img width="100%" height="100%" src='./loading.gif' alt=''>`;
+    setTimeout(() => {
+        document.querySelector(".loading").innerHTML = '<button onclick="handleClick()" class="clear">X</button>';
+    }, 1000);
+    const value = event.target.value;
     const data = Object.values(MAIN_ROUTES).flat();
-    console.log(data);
-
+    console.log(value);
     const results = data.filter((item) => {
-        const str = item.streetName;
-        const result1 = str.match(new RegExp(streetName, "i"));
-        const result2 = createInitials(str).match(new RegExp(streetName, "i"));
-        if (result1 == null && result2 == null) return false;
-        if (houseNumer < item.firstNumber || houseNumer > item.lastNumber) return false;
-        if (item.type == "Odd" && houseNumer % 2 == 0) return false;
-        if (item.type == "Even" && houseNumer % 2 == 1) return false;
-        return true
+        if (value.length == 0) {
+            return true;
+        }
+        if (/^[0-9]+\s.{0,}/.test(value)) {
+            // Trường hợp vừa số vừa chữ
+            const _value = value.split(" ").slice(1).join(" ");
+            const _number = value.split(" ")[0];
+            const result1 = item.streetName.match(new RegExp(_value, "i"));
+            const result2 = createInitials(item.streetName).match(new RegExp(_value, "i"));
+            if (item.streetName == "Hà Xuân 1") console.log("xuan hà 1", _value, result1, result2);
+            if (!result1 && !result2) return false;
+            if (_number < item.firstNumber || _number > item.lastNumber) return false;
+            if (item.type == "Odd" && _number % 2 == 0) return false;
+            if (item.type == "Even" && _number % 2 == 1) return false;
+            return true;
+        }
+        // Trường hợp chỉ có số
+        if (/^[0-9]+/.test(value)) {
+            const number = Number(value.match(/^[0-9]+/)[0]);
+
+            if (number < item.firstNumber || number > item.lastNumber) return false;
+            if (item.type == "Odd" && number % 2 == 0) return false;
+            if (item.type == "Even" && number % 2 == 1) return false;
+            return true;
+        }
+
+        if (/^[^0-9]/.test(value)) {
+            const result1 = item.streetName.match(new RegExp(value, "i"));
+            const result2 = createInitials(item.streetName).match(new RegExp(value, "i"));
+            if (result1 == null && result2 == null) return false;
+            return true;
+        }
+        //Trường hợp tìm theo giỏ
+        if (/^O[0-9]/.test(value)) {
+        }
     });
     document.querySelector(".data-list").innerHTML = results
-        .map((item) => { console.log(CODS['O10']); console.log(item.route) ; console.log(CODS.O10);
+        .map((item) => {
             return `
             <div class="item">
             <div class="userImage">
-                <img src="https://via.placeholder.com/50" alt="Profile Image">
+                <img src="https://hrchannels.com/Upload/avatar/20230325/144310892_Logo.png" alt="Profile Image">
             </div>
             <div class="info">
             <p class="street-name">${item.streetName}</p>
-                <p class="person-name">${CODS[item.route].name} - ${CODS[item.route].phone}</p>
+                <p class="person-name">${CODS[item.route]?.name} - ${CODS[item.route]?.phone}</p>
             </div>
         </div>
        `;
@@ -56,3 +89,24 @@ function handleInputChange(event) {
 // Gán sự kiện onChange cho input với hàm debounce
 const inputElement = document.getElementById("userInput");
 inputElement.addEventListener("input", debounce(handleInputChange, 500)); // 500ms
+
+function showData(){
+    document.querySelector(".data-list").innerHTML = Object.values(MAIN_ROUTES)
+        .flat()
+        .map((item) => {
+            return `
+            <div class="item">
+            <div class="userImage">
+                <img src="https://hrchannels.com/Upload/avatar/20230325/144310892_Logo.png" alt="Profile Image">
+            </div>
+            <div class="info">
+            <p class="street-name">${item.streetName}</p>
+                <p class="person-name">${CODS[item.route]?.name} - ${CODS[item.route]?.phone}</p>
+            </div>
+        </div>
+       `;
+        })
+        .join("");
+
+}
+showData()
